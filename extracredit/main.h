b34ac0b3dac93ac28ff2d3911ffcb0c9c58f051f
nezu <nezu@nezu.(none)>
2013-06-16 12:14:25 -0400

    commit1

---
+++ extracredit/main.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.h
+ * Author: nezu
+ *
+ * Created on February 23, 2013, 5:09 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+#define START_SIZE 10
+
+
+typedef char* string;
+
+
+
+#endif	/* MAIN_H */
